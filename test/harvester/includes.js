9137517bb264c214c3b72ac3fec71592a1343c19
Braden O'Guinn <braden.oguinn@mutualmobile.com>
2015-03-19 13:08:51 -0500

    [#414] Creates nested schema
    lib/adapters/mongodb.js
    - Updates schema links generation, deletes old ref
    
    lib/route.js
    - Makes patch use nuclear $set
    
    test/harvester/includes.js
    - Nests patch paths in links
    
    Signed-off-by: Braden O'Guinn <braden.oguinn@mutualmobile.com>

--- test/harvester/includes.js
+++ test/harvester/includes.js
@@ -25,11 +25,11 @@ module.exports = function(baseUrl,keys,ids) {
                 });
             }
             RSVP.all([
-                link('/people/' + ids.people[0], '/people/0/soulmate', ids.people[1]),
+                link('/people/' + ids.people[0], '/people/0/links/soulmate', ids.people[1]),
                 //TODO: harvester should take care about this on its own
-                link('/people/' + ids.people[1], '/people/0/soulmate', ids.people[0]),
+                link('/people/' + ids.people[1], '/people/0/links/soulmate', ids.people[0]),
 
-                link('/people/' + ids.people[0], '/people/0/lovers', [ids.people[1]])
+                link('/people/' + ids.people[0], '/people/0/links/lovers', [ids.people[1]])
             ]).then(function(){
                 done();
             });
