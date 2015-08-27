2528131a5e2f2dc50da0e779e6eb8beec492d30e
sajdakk <kristof.sajdak@gmail.com>
2015-08-27 23:18:50 +0200

    hardcoding docker host ip address as the default for now

--- test/config.js
+++ test/config.js
@@ -6,10 +6,10 @@ module.exports = {
         port: harvesterPort,
         options: {
             adapter: 'mongodb',
-            connectionString: process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/testDB",
+            connectionString: process.env.MONGODB_URL || "mongodb://192.168.59.103:27017/testDB",
             db: process.env.MONGODB || 'testDB',
             inflect: true,
-            oplogConnectionString: (process.env.OPLOG_MONGODB_URL || "mongodb://127.0.0.1:27017/local") + '?slaveOk=true'
+            oplogConnectionString: (process.env.OPLOG_MONGODB_URL || "mongodb://192.168.59.103:27017/local")
         }
     }
 };
--- test/util.js
+++ test/util.js
@@ -2,10 +2,10 @@ module.exports = {
     generateCustomHarvesterOptions :function(dbName){
         return {
             adapter: 'mongodb',
-            connectionString: "mongodb://127.0.0.1:27017/"+dbName,
+            connectionString: "mongodb://192.168.59.103:27017/"+dbName,
             db: dbName,
             inflect: true,
-            oplogConnectionString: "mongodb://127.0.0.1:27017/local?slaveOk=true"
+            oplogConnectionString: "mongodb://192.168.59.103:27017/local"
         }
     }
 };
